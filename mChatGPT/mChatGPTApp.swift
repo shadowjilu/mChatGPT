ddeca5a180bcb08ff8f2b631a687a910da019a4a
shadowjilu <shadowjilu@gmail.com>
2023-03-17 21:17:15 +0900

    First commit

---
+++ mChatGPT/mChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  mChatGPTApp.swift
+//  mChatGPT
+//
+//  Created by キロ on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct mChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
